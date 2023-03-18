428a589ee4cb363bddb8de30780fabf076737d01
Saketh Ganji <ganjisaketh@gmail.com>
2023-03-18 15:07:36 -0500

    updated all functions to work with piped commands

--- sish.c
+++ sish.c
@@ -5,12 +5,22 @@
 #include <sys/wait.h>
 
 #define ARGS_MAX 50
-
+#define PATH_MAX 256
+#define HISTORY_SIZE_MAX 100
 
 void readInput(char** inputString, size_t* len);
 void tokenizeInput(char* inputString, char*** args, int* pipeCount);
-void executeCommands(char*** args, int pipeCount);
+void changeDirectory(char*** args);
+int executeCommand(char* inputString, char*** args, int* pipeCount);
+void executableCommands(char*** args, int pipeCount);
+void addHistoryCommands(char* inputString);
+void history(char*** args, int* pipeCount);
+void executeHistoryOffset(int offset, int* pipeCount);
+void clearHistory();
+void printHistory();
 
+char* historyCommands[HISTORY_SIZE_MAX];
+int historyCount = 0;
 
 int main() {
 
@@ -22,29 +32,33 @@ int main() {
     while (1) {
         readInput(&inputString, &len);
 
-        tokenizeInput(inputString, args, &pipeCount);
+        int commandStatus = executeCommand(inputString, args, &pipeCount);
 
-        if (args[0] == NULL || args[0][0] == NULL) {
+        if (commandStatus == 1) {
             continue;
         }
-        else if (strcmp(args[0][0], "exit") == 0) {
+        else if (commandStatus == 2) {
             free(inputString);
             exit(EXIT_SUCCESS);
         }
-
-/*        for (int i = 0; i <= pipeCount; i++) {
-            printf("Pipe section %d:\n", i);
-            for (int j = 0; args[i][j] != NULL; j++) {
-                printf("\targs[%d][%d]: %s\n", i, j, args[i][j]);
-            }
-        }*/
-
-        executeCommands(args, pipeCount);
     }
 }
 
+void printPath() {
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("sish:%s> ", cwd);
+    }
+    else {
+        perror("getcwd()");
+        printf("sish> ");
+    }
+    fflush(stdout);
+}
+
 void readInput(char** inputString, size_t* len) {
-    printf("sish> ");
+    //printf("sish> ");
+    printPath();
 
     if (getline(inputString, len, stdin) == -1) {
         perror("getline");
@@ -54,6 +68,7 @@ void readInput(char** inputString, size_t* len) {
     if ((*inputString)[strlen(*inputString) - 1] == '\n') {
         (*inputString)[strlen(*inputString) - 1] = '\0';
     }
+    addHistoryCommands(*inputString);
 }
 
 void tokenizeInput(char* inputString, char*** args, int* pipeCount) {
@@ -90,7 +105,88 @@ void tokenizeInput(char* inputString, char*** args, int* pipeCount) {
     *pipeCount = pipeIndex - 1;
 }
 
-void executeCommands(char*** args, int pipeCount) {
+int executeCommand(char* inputString, char*** args, int* pipeCount) {
+    tokenizeInput(inputString, args, pipeCount);
+
+    if (args[0] == NULL || args[0][0] == NULL) {
+        return 1;
+    }
+    else if (strcmp(args[0][0], "exit") == 0) {
+        return 2;
+    }
+    else if (strcmp(args[0][0], "cd") == 0) {
+        changeDirectory(args);
+    }
+    else if (strcmp(args[0][0], "history") == 0) {
+        history(args, pipeCount);
+    }
+    else {
+        executableCommands(args, *pipeCount);
+    }
+
+    return 0;
+}
+
+
+void changeDirectory(char*** args) {
+    if (args[0][1] == NULL || strcmp(args[0][1], "~") == 0) {
+        chdir(getenv("HOME"));
+    } else {
+        if (chdir(args[0][1]) == -1) {
+            perror("sish: cd");
+        }
+    }
+}
+
+void addHistoryCommands(char* inputString) {
+    if (historyCount == HISTORY_SIZE_MAX) {
+        free(historyCommands[0]);
+        for (int i = 1; i < HISTORY_SIZE_MAX; i++) {
+            historyCommands[i-1] = historyCommands[i];
+        }
+        historyCount--;
+    }
+    historyCommands[historyCount] = strdup(inputString);
+    historyCount++;
+}
+
+void history(char*** args, int* pipeCount) {
+    if (args[0][1] != NULL && strcmp(args[0][1], "-c") == 0) {
+        clearHistory();
+    } else if (args[0][1] != NULL) {
+        int offset = atoi(args[0][1]);
+        executeHistoryOffset(offset, pipeCount);
+    } else {
+        printHistory();
+    }
+}
+
+void executeHistoryOffset(int offset, int* pipeCount) {
+    char* inputString;
+    char** args[ARGS_MAX + 1] = { NULL };
+    if (offset >= 0 && offset < historyCount) {
+        inputString = strdup(historyCommands[offset]);
+        executeCommand(inputString, args, pipeCount);
+        free(inputString);
+    } else {
+        printf("sish: history: invalid offset\n");
+    }
+}
+
+void clearHistory() {
+    for (int i = 0; i < historyCount; i++) {
+        free(historyCommands[i]);
+    }
+    historyCount = 0;
+}
+
+void printHistory() {
+    for (int i = 0; i < historyCount; i++) {
+        printf("%d %s\n", i, historyCommands[i]);
+    }
+}
+
+void executableCommands(char*** args, int pipeCount) {
     int fds[pipeCount][2];
 
     for (int i = 0; i < pipeCount; i++) {
@@ -102,10 +198,12 @@ void executeCommands(char*** args, int pipeCount) {
 
     int pid;
     for (int i = 0; i <= pipeCount; i++) {
+
         if ((pid = fork()) == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
-        } else if (pid == 0) {
+        }
+        else if (pid == 0) {
             if (i > 0) {
                 if (dup2(fds[i-1][0], STDIN_FILENO) == -1) {
                     perror("dup2");
@@ -161,3 +259,4 @@ void executeCommands(char*** args, int pipeCount) {
 
     }
 }
+
